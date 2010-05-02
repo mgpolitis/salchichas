6b8058b0329efafe972911ced792328fc3197a0d
williams.matias <williams.matias@4745d79f-2e93-7d13-d3e2-6067151deb5d>
2010-05-02 18:51:43 +0000

    Starting message

--- trunk/TPE/src/marshall/Message.java
+++ trunk/TPE/src/marshall/Message.java
@@ -1,5 +1,17 @@
 package marshall;
 
-public class Message {
 
+public abstract class Message {
+
+	public String host = null;
+	public Integer port = null;
+	
+	public abstract byte[] serialize();
+	
+	public Message(byte[] data) {
+		this.loadData(data);
+	}
+
+	public abstract void loadData(byte[] data);
+	
 }
