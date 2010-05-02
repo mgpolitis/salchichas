6750b2674c3e506a87bc604a245ce751b5faae21
manuelaraoz <manuelaraoz@4745d79f-2e93-7d13-d3e2-6067151deb5d>
2010-05-02 21:51:48 +0000

    correXión de un tremendo bug en el reactor. ahora manda los mensajes a quien corresponde

---
+++ TPE/src/marshall/EndPoint.java
@@ -0,0 +1,44 @@
+package marshall;
+
+public class EndPoint {
+
+	public String host;
+	public Integer port;
+	
+	public EndPoint(String host, Integer port) {
+		this.host = host;
+		this.port = port;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 97;
+		int result = 1;
+		result = prime * result + ((host == null) ? 0 : host.hashCode());
+		result = prime * result + ((port == null) ? 0 : port.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EndPoint other = (EndPoint) obj;
+		if (host == null) {
+			if (other.host != null)
+				return false;
+		} else if (!host.equals(other.host))
+			return false;
+		if (port == null) {
+			if (other.port != null)
+				return false;
+		} else if (!port.equals(other.port))
+			return false;
+		return true;
+	}
+	
+}
