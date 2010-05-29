e108219f238660c7fb5a6b93505cd77627ce2637
williams.matias <williams.matias@4745d79f-2e93-7d13-d3e2-6067151deb5d>
2010-05-29 20:32:43 +0000

    algunas mejoras poco a poco....

--- trunk/TPE/src/domain/data/WorkerDAO.java
+++ trunk/TPE/src/domain/data/WorkerDAO.java
@@ -1,12 +1,46 @@
 package domain.data;
 
+import pdclogs.LogsClient;
+import tgp.TGPClient;
+
 
 public class WorkerDAO {
 	private String directorIP = null;
-
+	private TGPClient tgpClient = null;
+	private LogsClient logsClient = null;
+	private StringBuffer logs =  null;
+	
 	public WorkerDAO(){
-		
+		logs = new StringBuffer();
+		tgpClient = new TGPClient("localhost","8091","localhost","8092",this);
+		logsClient = new LogsClient("localhost",8085,this);
 	}
+
+	public StringBuffer getLogs() {
+		return logs;
+	}
+
+	public void setLogs(StringBuffer logs) {
+		this.logs = logs;
+	}
+
+	public TGPClient getTgpClient() {
+		return tgpClient;
+	}
+
+	public void setTgpClient(TGPClient tgpClient) {
+		this.tgpClient = tgpClient;
+	}
+
+	public LogsClient getLogsClient() {
+		return logsClient;
+	}
+
+	public void setLogsClient(LogsClient logsClient) {
+		this.logsClient = logsClient;
+	}
+	
+	
 	
 	
 }
