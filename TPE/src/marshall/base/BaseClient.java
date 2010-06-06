c45dc2e45708efd5f6239f843ebf01a94af0280a
manuelaraoz <manuelaraoz@4745d79f-2e93-7d13-d3e2-6067151deb5d>
2010-06-06 13:59:59 +0000

    metodo para mandar mensajes

--- TPE/src/marshall/base/BaseClient.java
+++ TPE/src/marshall/base/BaseClient.java
@@ -1,11 +1,17 @@
 package marshall.base;
 
+import java.io.IOException;
 import java.util.List;
 
 import marshall.model.Message;
 
 public abstract class BaseClient {
 
+	
+	protected void sendMessage(Message m) throws IOException {
+		//TODO: do
+	}
+	
 	public abstract List<Message> messageReceived(Message m);
 	
 	public abstract Message greet();
--- TPE/src/marshall/base/BaseServer.java
+++ TPE/src/marshall/base/BaseServer.java
@@ -1,12 +1,18 @@
 package marshall.base;
 
+import java.io.IOException;
 import java.util.List;
 
 import marshall.model.Message;
 
 public abstract class BaseServer {
 
+	protected void sendMessage(Message m) throws IOException {
+		//TODO: do
+	}
+	
 	public abstract List<Message> messageReceived(Message m);
+	
 	public abstract Message createMessage(byte[] serialized);
 	
 }
