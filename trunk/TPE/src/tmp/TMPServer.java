7ed6ebf9865020dc0db70d081a14aada6997b88c
120686@gmail.com <120686@gmail.com@4745d79f-2e93-7d13-d3e2-6067151deb5d>
2010-06-06 21:35:00 +0000

    TMP medio cabezón. Falta implementar los pedidos de progreso analisis.

--- trunk/TPE/src/tmp/TMPServer.java
+++ trunk/TPE/src/tmp/TMPServer.java
@@ -1,27 +1,45 @@
 package tmp;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+
+import domain.services.DirectorService;
+import domain.services.WorkerService;
+
+import tmp.TMPMessage;
 
 import marshall.base.BaseServer;
+import marshall.model.EndPoint;
 import marshall.model.Message;
-import domain.services.WorkerService;
+
 
 
 public class TMPServer extends BaseServer{
 
+	DirectorService directorService;
 	WorkerService workerService;
 	
 	public static void main(String[] args) throws IOException {
 	
 	}
 	
-	public TMPServer(WorkerService workerService){
+	public TMPServer(DirectorService directorService){
 		super();
-		this.workerService = workerService;
+		this.directorService = directorService;
+		this.workerService = null;
+		
 	}
 
+	public TMPServer(WorkerService workerService){
+		super();
+		this.directorService = null;
+		this.workerService = workerService;
+	}
+	
 	@Override
 	public Message createMessage(byte[] serialized) {
 		TMPMessage message = new TMPMessage(serialized);
@@ -53,61 +71,90 @@ public class TMPServer extends BaseServer{
 	
 	private Message requestMessage(TMPMessage message) {
 		String serviceRequest = message.getService();
-		Message resp = null;
+		TMPMessage resp = null;
 		
 		if(!serviceRequest.isEmpty()){
 			int service = Integer.valueOf(serviceRequest);		
 		
 		
 			switch(service){
-				case 1: //TODO: cantidad de lineas
-					resp = lines();
+				case 1: 
+					resp = lines(resp);
 					break;
-				case 2: //TODO: cantidad de trabajos procesados
-					resp = jobs();
+					
+				case 2: 
+					resp = jobs(resp);
 					break;
-				case 3: //TODO: listado de trabajadores suscriptos
-					resp = workers();
+					
+				case 3: 
+					resp = workers(resp);
 					break;
-				case 4: //TODO: nodo director
-					resp = director();
+					
+				case 4: 
+					resp = director(resp);
 					break;
+					
 			}
 		}
 		else{
-			//TODO: Se pide todo.
-			resp = all();
+			resp = all(resp);
 		}
+		
+		if(directorService!=null){
+			Collection<String> c = new ArrayList<String>();
+			//si soy director agrego la informacion para de mis hijos
+			//TODO: List workers = this.directorService.getActiveWorkers();
+			
+			List<EndPoint> workers = null;
+			
+			String myWorkers = "";
+			
+			for(EndPoint ep : workers){
+				myWorkers += ep.host + "-" + ep.port;
+			}
+			
+			c.add(myWorkers);
+			resp.addContents(c);
+		}
+		
 		
 		return resp;
 	}
 	
-	private Message lines(){
-		TMPMessage message = null;
+	private TMPMessage lines(TMPMessage message){
+		
+		//TODO: this.workerService.getLinesProcessed();
 		
 		return message;
 	}
 	
-	private Message jobs(){
-		TMPMessage message = null;
+	private TMPMessage jobs(TMPMessage message){
+		
+		//TODO:this.workerService.getJobsDone();	
 		
 		return message;
 	}
 	
-	private Message workers(){
-		TMPMessage message = null;
+	private TMPMessage workers(TMPMessage message){
+		
 		
 		return message;
 	}
 	
-	private Message director(){
-		TMPMessage message = null;
+	private TMPMessage director(TMPMessage message){
+		
+		
+		//TODO:this.workerService.getDirector();
 		
 		return message;
 	}
 	
-	private Message all(){
-		TMPMessage message = null;
+	private TMPMessage all(TMPMessage message){
+		// MMM!!!!
+		message = lines(message);
+		message = jobs(message);
+		message = workers(message);
+		message = director(message);
 		
 		return message;	
 	}
