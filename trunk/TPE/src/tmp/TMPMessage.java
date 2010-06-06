e1c6a78e6116b09f58813c8d70416d52ffd56674
120686@gmail.com <120686@gmail.com@4745d79f-2e93-7d13-d3e2-6067151deb5d>
2010-06-06 13:36:24 +0000

    Agrego al control de versionado el TMP (a medias).

---
+++ trunk/TPE/src/tmp/TMPMessage.java
@@ -0,0 +1,109 @@
+package tmp;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import marshall.model.EndPoint;
+import marshall.model.Message;
+
+public class TMPMessage extends Message{
+
+	private String type = "";
+	private Map<String, String> content =  new HashMap<String, String>();
+	
+	public TMPMessage(EndPoint origin, EndPoint dest, String type, List<String> content) {
+		super(origin, dest, new byte[0]);
+		this.type = type;
+		addContents(content);
+	}
+	
+	public TMPMessage(String type, List<String> content){
+		this(null, null, type, content);
+	}
+	
+	public TMPMessage(byte[] serialized) {
+		super(null, null, serialized);
+		loadData(serialized);
+	}
+	
+	@Override
+	public void loadData(byte[] data) {
+		String dataString = new String(data);
+		if (!(dataString.isEmpty())) {
+			int messageSeparator = dataString.indexOf('\n');
+			if (messageSeparator != -1) {
+				this.type = dataString.substring(0, messageSeparator);
+				String[] tempArray = dataString.substring(messageSeparator)
+						.split("\\n\\s*\\n");
+				if (tempArray.length > 0) {
+					String[] splittedContent = tempArray[0].split("\\n");
+					for (String str : splittedContent) {
+						addContent(str);
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public byte[] serialize() {
+		String data = toString();
+		return data.getBytes();
+	}
+
+	private Map<String, String> addContent(String str) {
+		if (str != null) {
+			String[] tempArray = str.split(":");
+			if (tempArray.length == 2) {
+				this.content.put(tempArray[0], tempArray[1]);
+			} else {
+				System.out.println("invalid key-value: " + str);
+			}
+		} else {
+			System.out.println("invalid key-value, cannot be null ");
+		}
+		return content;
+	}
+	
+	public void addContents(Collection<String> contents) {
+		if (contents != null) {
+			Iterator<String> it = contents.iterator();
+			while (it.hasNext()) {
+				addContent(it.next());
+			}
+		}
+	}
+	
+	public String getContent(){
+		String contentString = "";
+		for (String str : this.content.keySet()) {
+			contentString += str + ":" + this.content.get(str) + '\n';
+		}
+		return contentString;
+	}
+	
+	public String getType(){
+		return type;
+	}
+	
+	public void setType(String type){
+		this.type = type;
+	}
+	
+	public String getService(){
+		return content.get("service");
+	}
+	
+	@Override
+	public String toString() {
+		String data = "";
+		data += this.type + '\n';
+		data += getContent() + '\n';
+		return data;
+	}
+
+	
+}
---
+++ trunk/TPE/src/tmp/TMPServer.java
@@ -0,0 +1,73 @@
+package tmp;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import tmp.TMPMessage;
+import marshall.interfaces.BaseServer;
+import marshall.model.EndPoint;
+import marshall.model.Message;
+
+
+public class TMPServer implements BaseServer{
+
+	
+	public static void main(String[] args) throws IOException {
+	
+	}
+
+	@Override
+	public Message createMessage(byte[] serialized) {
+		TMPMessage message = new TMPMessage(serialized);
+		return message;
+	}
+
+	@Override
+	public List<Message> messageReceived(Message m) {
+		List<Message> list = new LinkedList<Message>();
+		Message messageToSend = null;
+		if (m instanceof TMPMessage) {
+			System.out.println(m);
+			TMPMessage message = (TMPMessage) m;
+			System.out.println("Server: " + message);
+			
+			if (message.getType().equals("TMPREQUEST")) {
+				messageToSend = requestMessage(message);
+				
+			} else {
+				// TODO: unknown message
+			}
+		}
+		if (messageToSend != null) {
+			list.add(messageToSend);
+		}
+		return list;
+	}
+	
+	private Message requestMessage(TMPMessage message) {
+		String serviceRequest = message.getService();
+		Message resp = null;
+		
+		if(!serviceRequest.isEmpty()){
+			int service = Integer.valueOf(serviceRequest);		
+		
+		
+			switch(service){
+				case 1: //TODO: cantidad de lineas
+					break;
+				case 2: //TODO: cantidad de trabajos procesados
+					break;
+				case 3: //TODO: listado de trabajadores suscriptos
+					break;
+				case 4: //TODO: nodo director
+					break;
+			}
+		}
+		else{
+			//TODO: Se pide todo. 
+		}
+		
+		return resp;
+	}
+}
