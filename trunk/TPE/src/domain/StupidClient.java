6e35a60b827a792dcca4003dd75457e6d6ed3b0e
manuelaraoz <manuelaraoz@4745d79f-2e93-7d13-d3e2-6067151deb5d>
2010-06-09 12:47:45 +0000

    asd

--- trunk/TPE/src/domain/StupidClient.java
+++ trunk/TPE/src/domain/StupidClient.java
@@ -1,21 +1,49 @@
 package domain;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import marshall.Reactor;
 import wdp.FrontEndWDPClient;
 
 public class StupidClient {
 
-	public static void main(String[] args) throws IOException {
-		Reactor reactor = Reactor.getInstance();
+	private static BufferedReader r = new BufferedReader(new InputStreamReader(
+			System.in));
 
-		String serverHost = null; // TODO: obtener de linea de comando
-		FrontEndWDPClient c = new FrontEndWDPClient(serverHost,
+	public static void main(String[] args) throws IOException {
+
+		String serverHost = secureRead(
+				"Please enter the directors hostname or IP address:",
+				"Please enter a valid hostname");
+
+		Reactor reactor = Reactor.getInstance();
+		FrontEndWDPClient wdpClient = new FrontEndWDPClient(serverHost,
 				Configuration.WDP_SERVER_PORT);
-		reactor.subscribeTCPClient(c, "localhost",
+		reactor.subscribeTCPClient(wdpClient, serverHost,
 				Configuration.WDP_SERVER_PORT);
 		reactor.run();
+
+		String option = null;
+
+	}
+
+	private static String secureRead(String message, String errorMessage) {
+		String rta = null;
+		do {
+			System.out.println(message);
+			try {
+				rta = r.readLine();
+				if (rta == null) {
+					System.out.println(errorMessage);
+				}
+			} catch (IOException e) {
+				System.out.println("Error while reading from stdin");
+				System.exit(1);
+			}
+		} while (rta == null);
+		return rta;
 	}
 
 }
