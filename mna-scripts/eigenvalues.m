273fdd3df88c2ed565c27ddd5f56d0902085f9f6
manuelaraoz@gmail.com <manuelaraoz@gmail.com@4745d79f-2e93-7d13-d3e2-6067151deb5d>
2010-11-08 04:57:46 +0000

    householder y mas sobre eigen

--- mna-scripts/eigenvalues.m
+++ mna-scripts/eigenvalues.m
@@ -4,7 +4,19 @@ function u = eigenvalues()
 % C_{BE}^{-1} = C_{EB}
 
 
-% 
+% w = x-y/|x-y|2
+% P = I - 2 w w'
+
+% Ak = Pk Ak-1 Pk
+
+% faster:
+% V = Aw
+% c = w'V
+% Q = V - cw
+% PAP = A -2wQ'-2Qw'
+
+% Ak = Qk * Uk
+% Ak = Uk * Qk
 
 u=0;
 end
