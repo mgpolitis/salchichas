d3bf005f5adfc961aa897acf2b7f21e2f510c07d
manuelaraoz <manuelaraoz@4745d79f-2e93-7d13-d3e2-6067151deb5d>
2011-05-18 06:06:05 +0000

    super avances wiii funcion wordSet2list hecha

--- funcional/WordSet.hs
+++ funcional/WordSet.hs
@@ -7,16 +7,36 @@ where
     char2int :: Char -> Int
     char2int = ((ord 'a') `subtract`) . (ord)
 
+    int2char :: Int -> Char
+    int2char = (chr) . (+ (ord 'a'))
 
+    zipWithIndex :: [a] -> [(Int, a)]
+    zipWithIndex = innerZWI 0
+    
+    innerZWI :: Int -> [a] -> [(Int, a)]
+    innerZWI i [] = []
+    innerZWI i (x : xs) = (i, x) : (innerZWI (i+1) xs)   
 
-    data WordSet = GNode Bool [WordSet]
+    data GTree a = GNode a [WordSet] deriving (Eq)
+    type WordSet = GTree Bool
 
     subTreeForChar :: Char -> WordSet -> WordSet
     subTreeForChar c (GNode b ts) = ts !! (char2int c)
 
+    zipWithChar :: [a] -> [(Char, a)]
+    zipWithChar as = map (\(i,e) -> (int2char i, e)) (zipWithIndex as)
 
     vacio :: WordSet
     vacio = GNode False []
+
+    soloLambda :: WordSet
+    soloLambda = GNode True []    
+
+    newBranch :: [WordSet]
+    newBranch = replicate 26 vacio
+
+    newFullBranch :: [WordSet]
+    newFullBranch = replicate 26 soloLambda
 
     esVacio :: WordSet -> Bool
     esVacio (GNode False []) = True
@@ -25,21 +45,30 @@ where
     foldWordSet :: (Bool->c->b) -> ([b] -> c) -> WordSet -> b
     foldWordSet g h (GNode b ts) = g b (h (map (foldWordSet g h) ts ))
 
---     pertenece :: String -> WordSet -> Bool
---     pertenece [] =  sonIguales (GNode True [])
-    --pertenece (c : cs) = foldWordSet () ()
+    tamanio :: WordSet -> Int
+    tamanio = foldWordSet (step) (sum) where
+                step b = if b then (1+) else (0+)
+    
+
+    pertenece :: String -> WordSet -> Bool
+    pertenece [] =  sonIguales (GNode True [])
+    pertenece (c : cs) = \ws -> pertenece cs (subTreeForChar c ws)   
 
     agregarPalabra :: String -> WordSet -> WordSet
     agregarPalabra [] (GNode b ts) = GNode True ts
-    agregarPalabra (c : cs)= foldWordSet (Bool->c->WordSet) ([WordSet] -> c)
+    --agregarPalabra (c : cs)= foldWordSet (Bool->c->WordSet) ([WordSet] -> c)
 
     --borrarPalabra :: WordSet -> String -> WordSet
 
-    --cantidadQueEmpiezanCon :: WordSet -> String -> Integer
+    --cantidadQueEmpiezanCon :: WordSet -> String -> Int
 
---     sonIguales :: WordSet -> WordSet -> Bool
---     sonIguales vacio = esVacio
---     sonIguales (GNode b ts) = foldWordSet (\b2 hijos_son_iguales -> (b2 == b) && hijos_son_iguales) (and)
+    sonIguales :: WordSet -> WordSet -> Bool
+    sonIguales = (==)
 
-    -- wordSet2list :: WordSet -> [String]
+    wordSet2list :: WordSet -> [String]
+    wordSet2list (GNode b ts) = let zipped = (zipWithChar (map wordSet2list ts))
+                                    init = if b then [""] else []
+                                in init ++ (concat (map (\(c, l) -> map (c:) l) (zipped)))
+-- ++ (map (\(c,l) -> [c]:l) zipped )
+
 
