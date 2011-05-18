8a6d1ab81522d594668da688b859be7a3b20934f
manuelaraoz <manuelaraoz@4745d79f-2e93-7d13-d3e2-6067151deb5d>
2011-05-18 00:33:33 +0000

    avances?

--- funcional/WordSet.hs
+++ funcional/WordSet.hs
@@ -1,10 +1,19 @@
 module WordSet
     (WordSet, vacio, esVacio)
 where
+    import Char
 
     suma1 = (+1)
+    char2int :: Char -> Int
+    char2int = ((ord 'a') `subtract`) . (ord)
+
+
 
     data WordSet = GNode Bool [WordSet]
+
+    subTreeForChar :: Char -> WordSet -> WordSet
+    subTreeForChar c (GNode b ts) = ts !! (char2int c)
+
 
     vacio :: WordSet
     vacio = GNode False []
@@ -13,16 +22,24 @@ where
     esVacio (GNode False []) = True
     esVacio _ = False
 
-    --pertenece :: WordSet -> String -> Bool
+    foldWordSet :: (Bool->c->b) -> ([b] -> c) -> WordSet -> b
+    foldWordSet g h (GNode b ts) = g b (h (map (foldWordSet g h) ts ))
 
-    --agregarPalabra :: WordSet -> String -> WordSet
-    --agregarPalabra = foldWordSet ...
+--     pertenece :: String -> WordSet -> Bool
+--     pertenece [] =  sonIguales (GNode True [])
+    --pertenece (c : cs) = foldWordSet () ()
+
+    agregarPalabra :: String -> WordSet -> WordSet
+    agregarPalabra [] (GNode b ts) = GNode True ts
+    agregarPalabra (c : cs)= foldWordSet (Bool->c->WordSet) ([WordSet] -> c)
 
     --borrarPalabra :: WordSet -> String -> WordSet
 
     --cantidadQueEmpiezanCon :: WordSet -> String -> Integer
 
-    --sonIguales :: WordSet -> WordSet -> Bool
+--     sonIguales :: WordSet -> WordSet -> Bool
+--     sonIguales vacio = esVacio
+--     sonIguales (GNode b ts) = foldWordSet (\b2 hijos_son_iguales -> (b2 == b) && hijos_son_iguales) (and)
 
-wordSet2list :: WordSet -> [String]
+    -- wordSet2list :: WordSet -> [String]
 
