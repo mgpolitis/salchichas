ba0eed80a76daf5a101528c0cf7bf5c095c4b0ba
manuelaraoz <manuelaraoz@4745d79f-2e93-7d13-d3e2-6067151deb5d>
2011-05-23 18:18:51 +0000

    anda deletear y se exporta la API

--- funcional/Main.hs
+++ funcional/Main.hs
@@ -8,15 +8,40 @@ w2l vacio
 w2l (agregarPalabra "" vacio)
 -- [""]
 
-resp1 = sonIguales (GNode True newFullBranch) (GNode True [GNode True [] | i<- [1..26]])
+sonIguales (GNode True newFullBranch) (GNode True [GNode True [] | i<- [1..26]])
 -- True
-resp2 = w2l (GNode True newFullBranch)
+w2l (GNode True newFullBranch)
 -- ["","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-resp3 = cantidadQueEmpiezanCon "a" (GNode True newFullBranch)
+cantidadQueEmpiezanCon "a" (GNode True newFullBranch)
 -- 1
-resp4 = cantidadQueEmpiezanCon "" (GNode True newFullBranch)
+cantidadQueEmpiezanCon "" (GNode True newFullBranch)
 -- 27
-resp5 = cantidadQueEmpiezanCon "p" (GNode True newFullBranch)
+cantidadQueEmpiezanCon "p" (GNode True newFullBranch)
 -- 1
-resp6 = cantidadQueEmpiezanCon "pe" (GNode True newFullBranch)
+cantidadQueEmpiezanCon "pe" (GNode True newFullBranch)
 -- 0
+
+(agregarPalabra "pepe" (agregarPalabra "hola" vacio)) == (aPalabras ["hola", "pepe"] vacio)
+-- True
+w2l (aPalabras ["hola", "pepe", "a", "b", "c"] vacio)
+-- ["a","b","c","hola","pepe"]
+w2l (borrarPalabra "a" (aPalabras ["hola", "pepe", "a", "b", "c"] vacio))
+-- ["b","c","hola","pepe"]
+w2l (borrarPalabra "c" (borrarPalabra "b" (aPalabras ["hola", "pepe", "a", "b", "c"] vacio)))
+-- ["a","hola","pepe"]
+w2l (borrarPalabra "pepe" (aPalabras ["hola", "pepe", "a", "b", "c"] vacio))
+-- ["a","b","c","hola"]
+w2l (borrarPalabra "pep" (aPalabras ["hola", "pepe", "a", "b", "c"] vacio))
+-- ["a","b","c","hola","pepe"]
+w2l (borrarPalabra "" (aPalabras ["hola", "pepe", "a", "b", "c"] vacio))
+-- ["a","b","c","hola","pepe"]
+w2l (borrarPalabra "h" (aPalabras ["hola", "pepe", "a", "b", "c"] soloLambda))
+-- ["","a","b","c","hola","pepe"]
+w2l (borrarPalabra "" (aPalabras ["hola", "pepe", "a", "b", "c"] soloLambda))
+-- ["a","b","c","hola","pepe"]
+pertenece "" (borrarPalabra "" (aPalabras ["hola", "pepe", "a", "b", "c"] soloLambda))
+-- False
+pertenece "hola" (borrarPalabra "" (aPalabras ["hola", "pepe", "a", "b", "c"] soloLambda))
+-- True
+pertenece "hola" (borrarPalabra "hola" (aPalabras ["hola", "pepe", "a", "b", "c"] soloLambda))
+-- False
--- funcional/WordSet.hs
+++ funcional/WordSet.hs
@@ -1,8 +1,17 @@
 module WordSet
-    (WordSet, vacio, esVacio)
+    (WordSet, vacio, esVacio,
+    pertenece, agregarPalabra, borrarPalabra, aPalabras,
+    cantidadQueEmpiezanCon, tamanio,
+    wordSet2list, w2l,
+    sonIguales)
 where
     import Char
     import Maybe
+
+    -- ************
+    --  utilidades
+    -- ************
+
 
     suma1 = (+1)
     char2int :: Char -> Int
@@ -42,8 +51,8 @@ where
     newBranch :: [WordSet]
     newBranch = replicate 26 vacio
 
-    newBranchWithChar :: Char -> [WordSet]
-    newBranchWithChar c = setElement newBranch soloLambda (char2int c)
+--     newBranchWithChar :: Char -> [WordSet]
+--     newBranchWithChar c = setElement newBranch soloLambda (char2int c)
 
     replaceBranch :: WordSet -> Char -> WordSet -> WordSet
     replaceBranch (GNode b ts) c branch = (GNode b (setElement ts branch (char2int c)))
@@ -51,53 +60,69 @@ where
     newFullBranch :: [WordSet]
     newFullBranch = replicate 26 soloLambda
 
+
+    -- **********************
+    --  funciones exportadas
+    -- **********************
+
+    -- dice si el conjunto es vacio o no
     esVacio :: WordSet -> Bool
     esVacio (GNode False []) = True
     esVacio _ = False
 
+    -- esquema recursivo para conjuntos de palabras
     foldWordSet :: (Bool->c->b) -> ([b] -> c) -> WordSet -> b
     foldWordSet g h (GNode b ts) = g b (h (map (foldWordSet g h) ts ))
 
+    -- devuelve la cantidad de palabras en el conjunto
     tamanio :: WordSet -> Int
     tamanio = foldWordSet (step) (sum) where
                 step b = if b then (1+) else (0+)
 
+    -- dice si la palabra pertenece al conjunto
     pertenece :: String -> WordSet -> Bool
     pertenece [] =  sonIguales (GNode True [])
     pertenece (c : cs) = \ws -> case (subTreeForChar c ws) of
                                     Nothing -> False
                                     Just branch -> pertenece cs branch
 
+    -- agrega una palabra al conjunto
     agregarPalabra :: String -> WordSet -> WordSet
     agregarPalabra [] (GNode b ts) = GNode True ts
     agregarPalabra (c : cs) ws@(GNode b ts) = case (subTreeForChar c ws) of
                                     Nothing -> replaceBranch (GNode b newBranch) c (agregarPalabra cs vacio)
                                     Just branch -> replaceBranch ws c (agregarPalabra cs branch)
 
+    -- agrega todas las palabras de una lista a un conjunto
     aPalabras :: [String] -> WordSet -> WordSet
     aPalabras [] ws = ws
     aPalabras (str : strs) ws = aPalabras strs (agregarPalabra str ws)
 
+    -- borra una palabra del conjunto. si no esta, devuelve el conjunto original
     borrarPalabra ::  String -> WordSet -> WordSet
     borrarPalabra [] (GNode b ts) = GNode False ts
---     borrarPalabra (c : cs) ws = case (subTreeForChar c ws) of
---                                 Nothing -> ws
---                                 Just branch -> borrarPalabra cs branch
+    borrarPalabra (c : cs) ws@(GNode b ts) = case (subTreeForChar c ws) of
+                                    Nothing -> ws
+                                    Just branch -> replaceBranch ws c (borrarPalabra cs branch)
 
+    -- devuelve la cantidad de palabras del conjunto que empiezan con el prefijo dado
     cantidadQueEmpiezanCon :: String -> WordSet -> Int
     cantidadQueEmpiezanCon [] ws        = tamanio ws
     cantidadQueEmpiezanCon (c : cs) ws  = case (subTreeForChar c ws) of
                                         Nothing -> 0
                                         Just branch -> cantidadQueEmpiezanCon cs branch
 
+    -- dice si ambos conjuntos contienen las mismas palabras
     sonIguales :: WordSet -> WordSet -> Bool
     sonIguales = (==)
 
+    -- convierte el conjunto de palabras a una lista de palabras
     wordSet2list :: WordSet -> [String]
     wordSet2list (GNode b ts) = let zipped = (zipWithChar (map wordSet2list ts))
                                     init = if b then [""] else []
                                 in init ++ (concat (map (\(c, l) -> map (c:) l) (zipped)))
 
+    -- igual que wordSet2list, pero mas comodo
     w2l :: WordSet -> [String]
     w2l = wordSet2list
 
